4d51f64a083c9550942c7b6e8cd535051da673b7
AFuxy <zakygames701@gmail.com>
2024-08-13 19:31:05 +0100

    [~] added remove command

---
+++ commands/staff/remove.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder, PermissionsBitField } = require('discord.js');
+const fs = require('node:fs');
+const config = require('../../config.json');
+const index = require('../../index.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('remove')
+        .setDescription('Removes a game from the suggestions list.')
+        .addStringOption(option =>
+            option
+                .setName('game')
+                .setDescription('Game Title')
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageMessages),
+
+    async execute(interaction) {
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+            const embed = new EmbedBuilder()
+                .setColor(0xFF0000)
+                .setTitle('Error')
+                .setDescription('You do not have permission to use this command.')
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+            return;
+        }
+
+        const game = interaction.options.getString('game');
+
+        const data = fs.readFileSync('./suggestions.json', 'utf8');
+        const suggestions = JSON.parse(data);
+        if (suggestions[game.toLowerCase()]) {
+            delete suggestions[game.toLowerCase()];
+            fs.writeFileSync('./suggestions.json', JSON.stringify(suggestions));
+            const embed = new EmbedBuilder()
+                .setColor(0x00FF00)
+                .setTitle('Success!')
+                .setDescription('The game has been removed from the list.')
+                .setTimestamp();
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+            index.updateList();
+        } else {
+            const embed = new EmbedBuilder()
+                .setColor(0xFF0000)
+                .setTitle('Error')
+                .setDescription('That game is not in the list.')
+                .setTimestamp();
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+    }
+}
